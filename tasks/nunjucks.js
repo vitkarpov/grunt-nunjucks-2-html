5771b8d64cad4314891e7608636ab2a48e83c5e0
vitkarpov <viktor.s.karpov@gmail.com>
2015-04-15 22:51:14 +0300

    avoid sharing vars between templates

--- tasks/nunjucks.js
+++ tasks/nunjucks.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
 
         async.each(this.files, function(f, done) {
             var filepath = path.join(process.cwd(), f.src[0]);
-            var data = options.data || {};
+            var data = JSON.parse(JSON.stringify(options.data || {}));
 
             if (typeof options.preprocessData === 'function') {
                 data = options.preprocessData.call(f, data);
