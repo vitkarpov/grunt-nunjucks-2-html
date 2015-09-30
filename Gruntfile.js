111fe0f1df5c65a7b52e3d6dedfc1af208cdb2f3
Mr. Hyde <mr.hyde@2bad.me>
2015-09-30 23:14:35 +0200

    jscs replaced with standard, code style changed to follow it

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,38 @@
-var path = require('path');
+var path = require('path')
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
     jscs: {
-      src: ["tasks/nunjucks.js", "tests/**/*.js"],
+      src: ['tasks/nunjucks.js', 'tests/**/*.js'],
       options: {
-        config: ".jscsrc"
+        config: '.jscsrc'
       }
     },
     nunjucks: {
       options: {
         fooName: 'foo',
         data: grunt.file.readJSON('tests/data.json'),
-        preprocessData: function(data) {
-          data.page = path.basename(this.src[0], '.html');
-          return data;
+        preprocessData: function (data) {
+          data.page = path.basename(this.src[0], '.html')
+          return data
         },
-        configureEnvironment: function(env) {
-          var options = this.options();
-          env.addGlobal(options.fooName, 'bar');
+        configureEnvironment: function (env) {
+          var options = this.options()
+          env.addGlobal(options.fooName, 'bar')
         }
       },
       render: {
         files: {
-          'tests/base/_output.html' : ['tests/base/input.html'],
-          'tests/leaking-vars/_output1.html' : ['tests/leaking-vars/input1.html'],
-          'tests/leaking-vars/_output2.html' : ['tests/leaking-vars/input2.html']
+          'tests/base/_output.html': ['tests/base/input.html'],
+          'tests/leaking-vars/_output1.html': ['tests/leaking-vars/input1.html'],
+          'tests/leaking-vars/_output2.html': ['tests/leaking-vars/input2.html']
         }
       }
     }
-  });
+  })
 
-  grunt.loadNpmTasks("grunt-jscs");
-  grunt.loadTasks("tasks/");
+  grunt.loadNpmTasks('grunt-jscs')
+  grunt.loadTasks('tasks/')
 
-  grunt.registerTask("test", ["nunjucks"]);
+  grunt.registerTask('test', ['nunjucks'])
 }
