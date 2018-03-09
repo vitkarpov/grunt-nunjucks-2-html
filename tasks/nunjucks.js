86d168138149ee14504d3c9c6e41ce258597001c
ArmorDarks <git@lavrins.com>
2018-03-09 21:33:59 +0200

    refactor rendering handling
    * Fixed issue with race conditions caused by templates with async filters (and most likely with async loaders).
    
       Task finished before async templates actually were rendered.
    
    * Improved error handling
    
       The task will properly count and display successfully rendered and errored templates and fail if there were any errors.
    
       Note, that right now task will try to render _all_ templates, even with errors, and fail task only in the end if there were any errors during rendering. It was intentional behavior since it allows to see all errors of templates during a single run, though it might be annoying in large projects. Maybe we need to handle it with an option.
    
    * Simplified code

--- tasks/nunjucks.js
+++ tasks/nunjucks.js
@@ -12,10 +12,42 @@ const nunjucks = require('nunjucks')
 const chalk = require('chalk')
 const path = require('path')
 
+/**
+ * Promsify Nunjucks render
+ * @param {object} env    Nunjucks configured environment
+ * @param {string} src    Path to the template
+ * @param {object} [data] Template context
+ * @return {promise}
+ */
+const render = (env, src, data) => new Promise((resolve, reject) => {
+  env.render(src, data, (error, rendered) => {
+    if (error) return reject(error)
+    return resolve(rendered)
+  })
+})
+
+/**
+ * Prepare data by cloning it to avoid leaks and processing it
+ * through relevant function
+ * @param {object}   options                  Task options
+ * @param {object}   [options.data]           Template context
+ * @param {function} [options.preprocessData] Processor for context
+ * @param {object}   file                     Current `this.files` `file` instance
+ * @return {object} Prepared data
+ */
+const prepareData = (options, file) => {
+  let data = Object.assign({}, options.data)
+
+  if (options.data && typeof options.preprocessData === 'function') {
+    data = options.preprocessData.call(file, data)
+  }
+
+  return data
+}
+
 module.exports = function (grunt) {
   grunt.registerMultiTask('nunjucks', `Renders Nunjucks' templates to HTML`, function () {
-    // Declare async task
-    const completeTask = this.async()
+    const done = this.async()
 
     // Get options and set defaults
     const options = this.options({
@@ -27,112 +59,67 @@ module.exports = function (grunt) {
       noCache: true
     })
 
-    // Finish task if no files specified
-    if (!this.files.length) {
-      grunt.log.error('No files specified.')
+    const total = this.files.length
 
-      // Finish task — nothing we can do without specified files
-      return completeTask()
+    if (!total) {
+      grunt.log.error('No files specified.')
+      return done()
     }
 
-    // Warn in case of undefined data
     if (!options.data) {
       grunt.log.error(`Template's data is empty. Guess you've forget to specify data option.`)
     }
 
-    // Arm Nunjucks
     const env = nunjucks.configure(options.paths, options)
 
-    // Pass configuration to Nunjucks if specified
     if (typeof options.configureEnvironment === 'function') {
       options.configureEnvironment.call(this, env, nunjucks)
     }
 
-    // Get number of files
-    const totalFiles = this.files.length
-    // Start counter for number of compiled files
-    let countCompiled = 0
+    let success = 0
+    let errors = 0
 
-    // Run compilation asynchronously, wait for finish, then print results and complete task
-    const task = new Promise((resolve, reject) => {
-      // Iterate over all files' groups
-      this.files.forEach(file => {
-        // Set destination
-        let filedest = file.dest
+    const renderFiles = this.files.reduce((files, file) => {
+      let filedest = file.dest
 
-        // Check whether there are any source files
-        if (!file.src.length) {
-          grunt.log.error(`No source files specified for ${chalk.cyan(filedest)}.`)
-
-          // Skip to next file — nothing we can do without specified source files
-          return reject(new Error('For some destinations were not specified source files.'))
-        }
-
-        // Iterate over files' sources
-        file.src.forEach(src => {
-          // Construct absolute path to file for Nunjucks
-          let filepath = path.join(process.cwd(), src)
-
-          let data = {}
-          // Clone data
-          for (let i in options.data) {
-            if (options.data.hasOwnProperty(i)) {
-              data[i] = options.data[i]
-            }
-          }
-
-          // Preprocess data
-          if (options.data && typeof options.preprocessData === 'function') {
-            data = options.preprocessData.call(file, data)
-          }
-
-          // Asynchronously render templates with configurated Nunjucks environment
-          // and write to destination
-          env.render(filepath, data, (error, result) => {
-            // Catch errors, warn
-            if (error) {
-              grunt.log.error(error)
-              grunt.fail.warn('Failed to compile one of the source files.')
-              grunt.log.writeln()
-
-              // Prevent writing of failed to compile file, skip to next file
-              return reject(new Error('Failed to compile some source files.'))
-            }
-
-            // Write rendered template to destination
-            grunt.file.write(filedest, result)
-
-            // Debug process
-            grunt.verbose.ok(`File ${chalk.cyan(filedest)} created.`)
-            grunt.verbose.writeln()
-
-            countCompiled++
-          })
-        })
-      })
-
-      // Finish Promise
-      resolve()
-    })
-
-    // Print any errors from rejects
-    task.catch(error => {
-      if (error) {
-        grunt.log.writeln()
-        grunt.log.error(error)
-        grunt.log.writeln()
+      if (!filedest) {
+        grunt.log.error(`No dest file specified`)
+        done()
       }
-    })
 
-    // Log number of processed templates
-    task.then(success => {
-      // Log number of processed templates
-      let logType = (countCompiled === totalFiles) ? 'ok' : 'error'
-      let countCompiledColor = (logType === 'ok') ? 'green' : 'red'
-      grunt.log[logType](`${chalk[countCompiledColor](countCompiled)}/${chalk.cyan(totalFiles)} ${grunt.util.pluralize(totalFiles, 'file/files')} compiled.`)
-    })
+      if (!file.src || !file.src.length) {
+        grunt.log.error(`No source files specified for ${chalk.cyan(filedest)}.`)
+        done()
+      }
 
-    // Finish async task
-    completeTask()
+      const renders = file.src.map((src) => new Promise((resolve, reject) =>
+        render(env, path.resolve(src), prepareData(options, file))
+          .then((rendered) => {
+            grunt.file.write(filedest, rendered)
+            success++
+          })
+          .catch((error) => {
+            grunt.log.error(error)
+            errors++
+          })
+          .then(() => resolve())
+      ))
+
+      return files.concat(renders)
+    }, [])
+
+    Promise.all(renderFiles)
+      .then(() => {
+        const isWrong = success !== total
+        const logColor = isWrong ? 'error' : 'ok'
+        const totalColor = isWrong ? 'red' : 'green'
+
+        grunt.log[logColor](`${chalk[totalColor](success)}/${chalk.cyan(total)} files rendered`)
+
+        if (isWrong) throw new Error(`Failed to compile ${errors} ${grunt.util.pluralize(errors, 'file/files')}.\nSee log above for details.`)
+
+        done()
+      })
+      .catch((error) => done(error))
   })
 }
